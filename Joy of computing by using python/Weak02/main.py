621a61bd08e1f837ceec3975b7cef2ae5a5fd31b
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-15 03:55:51 +0530

    done with 5th videos of weak 2

--- Joy of computing by using python/Weak02/main.py
+++ Joy of computing by using python/Weak02/main.py
@@ -1,4 +1,11 @@
 # ******* weak 2 code ********
 
-print("Printing random stuff through print function.")
-print("learned how to use print function in python for printing different charset.")
+# print("Printing random stuff through print function.")
+# print("learned how to use print function in python for printing different charset.")
+
+variable = "understand variable in python."
+print(variable)
+print(type(variable))
+var2 = "amanjeet"
+var3 = variable + " " + var2
+print(var3)
