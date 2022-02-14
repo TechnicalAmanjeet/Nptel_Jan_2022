b771abfeca79a43f0ae17b4ed753b4a1d197e883
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-15 04:11:12 +0530

    done with how to take input from user through console.

--- Joy of computing by using python/Weak02/main.py
+++ Joy of computing by using python/Weak02/main.py
@@ -3,6 +3,8 @@
 # print("Printing random stuff through print function.")
 # print("learned how to use print function in python for printing different charset.")
 
+# ******* working with variables *********
+
 # variable = "understand variable in python."
 # print(variable)
 # print(type(variable))
@@ -10,13 +12,20 @@
 # var3 = variable + " " + var2
 # print(var3)
 
-a = 10
-print(a)
-a += 1
-print(a)
-a += 1
-print(a)
-a += 1
-print(a)
-a += 1
-print(a)
+
+
+# a = 10
+# print(a)
+# a += 1
+# print(a)
+# a += 1
+# print(a)
+# a += 1
+# print(a)
+# a += 1
+# print(a)
+
+# ******** taking input from user. ********
+
+user_input = input("write your name : ")
+print("Your name is :", user_input)
