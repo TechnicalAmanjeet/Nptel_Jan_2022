9485093d8ce2cb3736368413149ee4e45759be81
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-15 19:48:18 +0530

    done with slicing in list

--- Joy of computing by using python/Weak03/main.py
+++ Joy of computing by using python/Weak03/main.py
@@ -34,13 +34,29 @@ myList1 = ["Amanjeet", "Ram", "Riya", "Kajal"]
 #
 # print(myList1.count("Neha"))  # print how many times neha is inside the list
 
+
 # ********* 3. List operations. ***********
-myList1.sort()  # sort the list in ascending order
-print(myList1)
 
-myList1.reverse()  # reverse the list
-print(myList1)
+# myList1.sort()  # sort the list in ascending order
+# print(myList1)
+# myList1.insert(1, "Kismat")  # insert kismat at the index 1.
+# print(myList1)
+#
+# myList1.reverse()  # reverse the list
+# print(myList1)
 
-myList1.insert(1, "Kismat")  # insert kismat at the index 1.
-print(myList1)
+
+# ******* 4. Slicing ***********
+
+print(myList1[:])  # default value => mylist1[:] == mylist1 => True
+print(myList1 == myList1[:])
+
+# slicing syntax => list[start_index : end_index + 1]
+# by using above syntax we will return a list which start from start_index element and goes till the end index element
+first_3_element = myList1[:3]  # print first 3 element of list
+print(first_3_element)
+
+last_3_element = myList1[len(myList1)-3 : ]
+# print(myList1)
+print(last_3_element)
 
