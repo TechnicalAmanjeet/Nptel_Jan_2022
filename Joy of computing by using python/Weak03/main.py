e97508ee1deb2ea7a328882927fc2e6d4b8b66f5
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-15 19:18:55 +0530

    done with basics of list datatype

--- Joy of computing by using python/Weak03/main.py
+++ Joy of computing by using python/Weak03/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+# ********* weak 3 practice **********
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# List datatype in python.
+# myList = range(11)  # create a list of number from 0 to 10 and stores in myList variable.
+myList = [1, "Amanjeet", "Ram", "shyam"]
+# print(myList)
 
+# we can iterate through loop as well.
+# for element in myList:
+#     print(element)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# list datatype contains following properties.
+# print(dir(list))
+# print(len(dir(list)))
