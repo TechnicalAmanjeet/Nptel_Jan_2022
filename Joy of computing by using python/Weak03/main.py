fcc3410446e3f050bbf60e6ea734ea79f3927143
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-15 19:33:51 +0530

    done with list manipulation and some of inbuilt methods which list has.

--- Joy of computing by using python/Weak03/main.py
+++ Joy of computing by using python/Weak03/main.py
@@ -12,3 +12,29 @@ myList = [1, "Amanjeet", "Ram", "shyam"]
 # list datatype contains following properties.
 # print(dir(list))
 # print(len(dir(list)))
+
+# ******** 2. List manipulation in python ********
+myList1 = ["Amanjeet", "Ram", "Riya", "Kajal"]
+myList1.append("Neha")  # add neha at the end of the list.
+print(myList1)
+
+myList1.insert(1, "Kismat")  # insert kismat at the index 1.
+print(myList1)
+
+last_element_of_list = myList1.pop()  # remove last element of list and return that element
+print(myList1)
+print(last_element_of_list)
+
+myList1.remove("Ram")  # remove the element "Ram" is it's inside the list
+print(myList1)
+# print(element_after_remove)
+
+myList1.sort()  # sort the list in ascending order
+print(myList1)
+
+myList1.reverse()  # reverse the list
+print(myList1)
+
+print(len(myList1))  # print how many element in the list.
+
+print(myList1.count("Neha"))  # print how many times neha is inside the list
