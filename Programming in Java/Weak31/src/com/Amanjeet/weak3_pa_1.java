a08dbf098520dc15634f814c33e0158db8b26fae
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-16 22:23:40 +0530

    done with weak 3 pa 2

--- Programming in Java/Weak31/src/com/Amanjeet/weak3_pa_1.java
+++ Programming in Java/Weak31/src/com/Amanjeet/weak3_pa_1.java
@@ -1,2 +1,18 @@
-package com.Amanjeet;public class weak3_pa_1 {
+package com.Amanjeet;
+
+public class weak3_pa_1 {
+
+    class Point{
+        double x,y;
+
+        public void distance(Point p1, Point p2){
+            double x1 = p1.x, x2 = p2.x, y1=p1.y, y2 = p2.y;
+            double first = x2 -x1, second = y2-y1;
+            first = first * first;
+            second = second * second;
+            double distance = Math.sqrt(first + second);
+            System.out.println(distance);
+        }
+    }
+
 }
