1e1863c94618f30319e89de437a7be4a035db713
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-18 05:53:38 +0530

    done with by taking cmd line input for args[] array

---
+++ Programming in Java/Weak2/src/com/Lecture_practice/cmd_line_input.java
@@ -0,0 +1,9 @@
+package com.Lecture_practice;
+
+public class cmd_line_input {
+    public static void main(String[] args) {
+        for(int index = 0; index < args.length; index++){
+            System.out.print(args[index]);
+        }
+    }
+}
