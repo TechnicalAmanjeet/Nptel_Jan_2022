6f3df40e00d58677c89ccc7b60bf68ca023ed38d
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-18 06:36:49 +0530

    solved quiz assignment 1 and renamed some file

---
+++ Programming in Java/Weak2/src/com/Quiz_Assignment/Q1.java
@@ -0,0 +1,9 @@
+package com.Quiz_Assignment;
+
+public class Q1 {
+    public static void main(String[] args) {
+        char a = '8';
+        int b = 010;
+        System.out.println(a+b);
+    }
+}
