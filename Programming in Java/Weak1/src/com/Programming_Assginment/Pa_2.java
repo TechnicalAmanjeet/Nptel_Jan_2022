855332f19430ed9e8142a2a7ae6a287060040522
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-19 02:01:08 +0530

    done with java weak1 pa 2

--- Programming in Java/Weak1/src/com/Programming_Assginment/Pa_2.java
+++ Programming in Java/Weak1/src/com/Programming_Assginment/Pa_2.java
@@ -1,2 +1,17 @@
-package com.Programming_Assginment;public class Pa_2 {
+package com.Programming_Assginment;
+
+
+import java.util.Scanner;
+
+public class Pa_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int result = 0;
+//Use if...else ladder to find the largest among 3 numbers and store the largest number in a variable called result.
+        result = Math.max(Math.max(x,y),z);
+        System.out.print(result);
+    }
 }
