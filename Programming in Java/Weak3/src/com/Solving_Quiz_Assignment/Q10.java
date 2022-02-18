99d54c0084f255bebff778b4354dd1e2eaedef87
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-19 02:27:51 +0530

    done with java weak 3 all quiz assignment

--- Programming in Java/Weak3/src/com/Solving_Quiz_Assignment/Q10.java
+++ Programming in Java/Weak3/src/com/Solving_Quiz_Assignment/Q10.java
@@ -1,2 +1,7 @@
-package com.Solving_Quiz_Assignment;public class Q10 {
+package com.Solving_Quiz_Assignment;
+
+public class Q10 {
+    public static void main(String[] args) {
+        System.out.println("All");
+    }
 }
