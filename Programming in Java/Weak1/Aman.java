f9e3b9d7cc1edbc0015a7f6a2f1f40803a3091af
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-04-02 01:39:17 +0530

    done with how to compile and run java program file

---
+++ Programming in Java/Weak1/Aman.java
@@ -0,0 +1,5 @@
+public class Aman {
+    public static void main(String[] args) {
+        System.out.println("Amanjeet loves riya so much..");
+    }
+}
---
+++ Programming in Java/Weak1/src/com/Lecture_prectice/Main.java
@@ -0,0 +1,7 @@
+package com.Lecture_prectice;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("started revising my nptel java course.");
+    }
+}
