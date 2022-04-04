4f3746d906bf98829a478ea1fd1e69f4401c7d7c
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-04-05 02:11:52 +0530

    added ss on java

--- Programming in Java/Weak2/src/com/Lecture_practice/Main.java
+++ Programming in Java/Weak2/src/com/Lecture_practice/Main.java
@@ -1,6 +1,6 @@
 package com.Lecture_practice;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         // System.out.println("Start writing cmd from here onwards.");
 
--- Programming in Java/Weak3/src/com/inheritance/Main.java
+++ Programming in Java/Weak3/src/com/inheritance/Main.java
@@ -2,6 +2,7 @@ package com.inheritance;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("starting with inheritance eg.");
     }
 }
