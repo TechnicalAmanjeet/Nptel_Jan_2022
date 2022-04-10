d2c00758131a2fd7137487158d2aff701a92ed61
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-04-11 00:02:28 +0530

    done with bubble sort

--- Joy of computing by using python/Weak05/prectice/sorting/bubble_sort.py
+++ Joy of computing by using python/Weak05/prectice/sorting/bubble_sort.py
@@ -1,1 +1,18 @@
-# implementationof
+# implementation of bubble sort.
+
+unsorted_list = [2, 1, 5, 3, 7, 6, 8, 4, 9]
+
+
+def swap(unsorted_list, i, j):
+    temp = unsorted_list[i]
+    unsorted_list[i] = unsorted_list[j]
+    unsorted_list[j] = temp
+
+
+for i in range(1, len(unsorted_list)):
+    for j in range(i, len(unsorted_list)):
+        if unsorted_list[i-1] > unsorted_list[j]:
+            swap(unsorted_list, i-1, j)
+            print(unsorted_list)
+
+print(unsorted_list)
