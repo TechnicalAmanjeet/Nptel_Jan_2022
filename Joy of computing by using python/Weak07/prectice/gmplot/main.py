5be87164c420a1368fd04578b1db5de109f60222
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-04-12 01:07:36 +0530

    done with small project by using google map

--- Joy of computing by using python/Weak07/prectice/gmplot/main.py
+++ Joy of computing by using python/Weak07/prectice/gmplot/main.py
@@ -4,21 +4,50 @@ import gmplot
 import csv
 import pandas as pd
 
-# method 1 to read csv file.
+# # method 1 to read csv file.
+# with open("./long_lat.csv", 'r') as file:
+#     reader = csv.reader(file)
+#     for data in reader:
+#         # print(data)
+#         pass
+#
+# # method 2 to read csv file
+# file1 = pd.read_csv("long_lat.csv")
+# numpy_array = file1.to_numpy()
+# longitude = []
+# latitude = []
+#
+# for item in numpy_array:
+#     longitude.append(item[0])
+#     latitude.append(item[0])
+
+
+gmap = gmplot.GoogleMapPlotter(28.356, 76.456, 15)
+
+gmap.coloricon = "http://www.googlemapsmarkers.com/v1/%s"
 with open("./long_lat.csv", 'r') as file:
     reader = csv.reader(file)
-    for data in reader:
-        # print(data)
-        pass
+    temp = 0
+    times = 0
 
-# method 2 to read csv file
-file1 = pd.read_csv("long_lat.csv")
-numpy_array = file1.to_numpy()
-longitude = []
-latitude = []
+    for row in reader:
+        try:
+            lat = float(row[0])
+            long = float(row[1])
+        except:
+            continue
 
-for item in numpy_array:
-    longitude.append(item[0])
-    latitude.append(item[0])
+        if temp == 0:
+            gmap.marker(lat, long, "blue")
+            temp = 1
+        else:
+            gmap.marker(lat, long, "yellow")
+
+        times += 1
+        if times == 500:
+            break
+
+gmap.marker(lat, long, "red")
+gmap.draw("mymap.html")
 
 
